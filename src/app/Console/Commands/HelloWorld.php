357915c0e80e83e14db3ca257ed55a14aba565ad
takyafumin <takyafumin@gmail.com>
2024-03-02 19:55:48 +0900

    テスト用Artisan Command作成

---
+++ src/app/Console/Commands/HelloWorld.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class HelloWorld extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'print:helloworld';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->line('Hello' . config('app.world'));
+
+        return 0;
+    }
+}
